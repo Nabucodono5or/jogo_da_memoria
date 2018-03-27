0e8976e360713c8dedd2194171e21f7cd9dcf9a9
Jefferson Abreu <pereirajefferson06@gmail.com>
2018-03-27 10:20:28 -0300

    adicionado novo snippet de teste.js

--- teste.js
+++ teste.js
@@ -1,5 +1,37 @@
 (function(){
-  for(let i = 1; i < 16; i+=2){
-    console.log(i);
+  var posicoesImagens = [];
+
+  function gerarRamdomValues() {
+    let min = 0;
+    let max = 7;
+
+    return Math.round(Math.random() * (max - min) + min);
   }
+
+  function repetido(array, indice){
+    let copia = 0;
+
+    for(let j = 0; j < indice; j++){
+      if(array[j] === array[indice]){
+        copia++;
+      }
+    }
+
+    if(copia > 1){
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  function gerarRamdomFotos(){
+    for(let i = 0; i < 16; i++){
+      do {
+        posicoesImagens[i] = gerarRamdomValues();
+      } while (repetido(posicoesImagens, i));
+    }
+    console.log(posicoesImagens);
+  }
+
+  gerarRamdomFotos();
 })();
